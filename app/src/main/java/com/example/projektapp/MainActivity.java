95fdc612b4e6572e44a7a814a6270bf3eb50cd12
a19meram <a19meram@student.his.se>
2021-04-17 19:46:47 +0200

    Adding a toast for target group!

--- app/src/main/java/com/example/projektapp/MainActivity.java
+++ app/src/main/java/com/example/projektapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //first button for page shifting
         Button home_button = findViewById(R.id.home_button);
         home_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intenthome = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intenthome);
+            }
+        });
+
+        //button for a toast that will stand for about page
+        Button about_button = findViewById(R.id.about_button);
+        about_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Detta sida har målgruppen...", Toast.LENGTH_LONG).show();
             }
         });
     }
