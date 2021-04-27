923e338c700f3c6c5000b358ebd2181101fef137
a19meram <a19meram@student.his.se>
2021-04-27 10:25:36 +0200

    adding a jsonTask and a new class for stadio!

--- app/src/main/java/com/example/projektapp/SecondActivity.java
+++ app/src/main/java/com/example/projektapp/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.projektapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,6 +15,17 @@ import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE;
@@ -52,5 +66,68 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
         make.show();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+
+            try {
+                adapter.clear();
+                JSONArray jsonArray = new JSONArray(json);
+                for (int i = 0; i < jsonArray.length(); i++){
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String name = jsonObject.getString("name");
+                    String location = jsonObject.getString("location");
+                    int size = jsonObject.getInt("size");
+
+
+                    stadioArrayList.add(new Stadio(name, size, location));
+                }
+                adapter.notifyDataSetChanged();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.d("TAG", json);
+        }
     }
 }
--- app/src/main/java/com/example/projektapp/Stadio.java
+++ app/src/main/java/com/example/projektapp/Stadio.java
@@ -1,4 +1,31 @@
 package com.example.projektapp;
 
 public class Stadio {
+    private String name;
+    private int size;
+    private String location;
+
+    public Stadio(String name, int size, String location) {
+        this.name = name;
+        this.size = size;
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+
+    }
+
+    public String getName(String name) {
+        return this.name;
+    }
+
+    public int getSize(String size) {
+        return this.size;
+    }
+
+    public String getLocation(String location) {
+        return this.location;
+    }
 }
